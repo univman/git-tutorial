032a521aefc1227377640e2eca89e2aea00b9d67
Stefan 2 Zajavkowy <stefan2@zaj.avk>
2023-03-24 12:05:55 +0100

    feat: [TASK-0006] mod Person.java

--- Person.java
+++ Person.java
@@ -5,9 +5,9 @@ public class Person {
         System.out.println("Hello2!");
         System.out.println("Hello4!");
         System.out.println("Hello5!");
-        System.out.println("Hello7!");
-        System.out.println("Hello7!");
-        System.out.println("Hello7!");
+        System.out.println("Hello8!");
+        System.out.println("Hello8!");
+        System.out.println("Hello8!");
         System.out.println("Hello7!");
     }
 }
