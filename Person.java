1f76912e2136abd7be243dc941a1f431136b9989
Roman Zajavkowy <roman@zaj.avk>
2023-03-24 13:25:50 +0100

    Person.java updated

--- Person.java
+++ Person.java
@@ -2,12 +2,5 @@ public class Person {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
-        System.out.println("Hello2!");
-        System.out.println("Hello4!");
-        System.out.println("Hello5!");
-        System.out.println("Hello8!");
-        System.out.println("Hello8!");
-        System.out.println("Hello8!");
-        System.out.println("Hello7!");
     }
 }
